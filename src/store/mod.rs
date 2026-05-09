75cc596dbb2cfb4be1ff715022349c762a9eecca
Christoph Herzog <chris@theduke.at>
2026-05-09 18:55:16 +0200

    Add async store trait for item persistence

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,1 +1,12 @@
 pub mod types;
+
+use async_trait::async_trait;
+
+pub use types::Item;
+
+#[async_trait]
+pub trait Store: Send + Sync {
+    async fn load_items(&self) -> anyhow::Result<Vec<Item>>;
+
+    async fn store_items(&self, items: Vec<Item>) -> anyhow::Result<()>;
+}
